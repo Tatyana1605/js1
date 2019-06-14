5e1e022359cc950e1bd80458c4e8399c69ba6357
Tatyana1605 <tiponomareva88@gmail.com>
2019-06-14 16:32:23 +0700

    4 commit

--- js/app1.js
+++ js/app1.js
@@ -1,11 +1,24 @@
 'use strict';
 
-for (let i = 0; i<= 10; i++) {
-  if (i == 0) {
-    console.log( i + "- это ноль");
-  } else if (i % 2) {
-    console.log( i + "- нечетное число");
-  } else {
-    console.log( i + "- четное число");
-  }
-}
+ function getNumber() {
+   let numberX = prompt("Введите число от 1 до 999, значение должно быть целым числом");
+   if (Number.isInteger(numberX) || numberX > 0 && numberX < 999) {
+     alert("Ваше число в диапазоне чисел");
+
+     return {
+      units:  numberX % 10,
+      tens:  Math.floor(numberX / 10) % 10,
+      hundreds: Math.floor(numberX / 100),
+     };
+
+   } else {
+     alert('Введеное вами число не в диапазоне чисел от 1 до 999');
+     return {};
+   }
+ };
+
+ let result = getNumber();
+
+ alert(`${result.units} //это единицы
+        ${result.tens} //это десятки
+        ${result.hundreds} //это сотни `);
--- js/app6.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var hill = "x";
-  console.log(hill);
-
-for(var x = 0; x <= 20; x++){
-  console.log(hill += "xx");
-}
